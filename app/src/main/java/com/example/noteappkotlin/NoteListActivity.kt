9478064c035b67c8468d794dbc9fb8c61ee02e0d
OTTIN-T <ottin.timoth@gmail.com>
2023-03-01 12:11:25 +0100

    Add Note into the activity

--- app/src/main/java/com/example/noteappkotlin/NoteListActivity.kt
+++ app/src/main/java/com/example/noteappkotlin/NoteListActivity.kt
@@ -2,10 +2,34 @@ package com.example.noteappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class NoteListActivity : AppCompatActivity() {
+class NoteListActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var notes: MutableList<Note>
+    lateinit var adapter: NoteAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
+
+        notes = mutableListOf<Note>()
+        notes.add(Note("Note 1", "C'est quoi un recycle view ?"))
+        notes.add(Note("Mémo A", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non fermentum nunc, id vehicula erat. Ut in pharetra lectus. Aliquam consectetur interdum eros, a fermentum justo sagittis et. Sed et erat vitae ex tempor condimentum."))
+        notes.add(Note("Mémo B", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non fermentum nunc, id vehicula erat. Ut in pharetra lectus. Aliquam consectetur interdum eros, a fermentum justo sagittis et. Sed et erat vitae ex tempor condimentum."))
+        notes.add(Note("Dernier mémo", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non fermentum nunc, id vehicula erat. Ut in pharetra lectus. Aliquam consectetur interdum eros, a fermentum justo sagittis et. Sed et erat vitae ex tempor condimentum."))
+
+        adapter = NoteAdapter(notes, this)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.notes_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+    }
+
+    override fun onClick(view: View?) {
+        if(view?.tag != null) {
+            Log.i("NoteActivity", "Note de ma list")
+        }
     }
 }
